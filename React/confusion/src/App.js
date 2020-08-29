326206abb2954943eb39b629994c1c06b252fc0a
Afiz Savage <savageafiz9@gmail.com>
2020-08-29 08:08:00 +0000

    components part 1

--- React/confusion/src/App.js
+++ React/confusion/src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./App.css";
 import { Navbar, NavbarBrand } from "reactstrap";
+import Menu from "./components/MenuComponent";
 function App() {
   return (
     <div className="App">
@@ -9,6 +10,7 @@ function App() {
           <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
         </div>
       </Navbar>
+      <Menu />
     </div>
   );
 }
