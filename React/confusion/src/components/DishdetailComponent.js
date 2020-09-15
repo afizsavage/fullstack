626165bd6c43c6624a0ba81fe5c3c22eec075343
Afiz Savage <yodusav@gmail.com>
2020-09-15 17:39:45 +0000

    react assignment 1 comments issue

--- React/confusion/src/components/DishdetailComponent.js
+++ React/confusion/src/components/DishdetailComponent.js
@@ -1,11 +1,5 @@
-import React, { Component  } from "react";
-import {
-  Card,
-  CardImg,
-  CardText,
-  CardBody,
-  CardTitle,
-} from "reactstrap";
+import React, { Component } from "react";
+import { Card, CardImg, CardText, CardBody, CardTitle } from "reactstrap";
 
 class DishDetail extends Component {
   renderDish(dish) {
@@ -22,31 +16,32 @@ class DishDetail extends Component {
     else return <div></div>;
   }
 
-<<<<<<< HEAD
   renderComments(commnts) {
-     commnts = this.props.comments.map((comnt) => {
-    return(<div><ul className="list-unstyled"><li key={comnt.id}>{comnt.author}</li></ul></div>)
+    commnts = this.props.comments.map((comnt) => {
+      return (
+        <div>
+          <ul className="list-unstyled">
+            <li key={comnt.id}>{comnt.comment}</li>
+            <li>
+              {comnt.author}, {Date.parse(comnt.date)}
+            </li>
+          </ul>
+        </div>
+      );
     });
-  return(<div className="col-12 col-md-5 m-1">{commnts}</div>)
+    return <div className="col-12 col-md-5 m-1">{commnts}</div>;
   }
 
-=======
->>>>>>> 5a94c9b46a05f53077a4352e1d0ebbd1dde27592
-  render(){
-    return(<div className="row">
-    <div className="col-12 col-md-5 m-1">
-      {this.renderDish(this.props.selectedDish)}
-    </div>
-<<<<<<< HEAD
-    {this.renderComments()}
-=======
->>>>>>> 5a94c9b46a05f53077a4352e1d0ebbd1dde27592
-  </div>)
+  render() {
+    return (
+      <div className="row">
+        <div className="col-12 col-md-5 m-1">
+          {this.renderDish(this.props.selectedDish)}
+        </div>
+        {this.renderComments()}
+      </div>
+    );
   }
 }
 
-<<<<<<< HEAD
-
-=======
->>>>>>> 5a94c9b46a05f53077a4352e1d0ebbd1dde27592
 export default DishDetail;
--- React/confusion/src/components/MenuComponent.js
+++ React/confusion/src/components/MenuComponent.js
@@ -1,11 +1,6 @@
 import React, { Component } from "react";
-import DishDetail from './DishdetailComponent'
-import {
-  Card,
-  CardImg,
-  CardImgOverlay,
-  CardTitle,
-} from "reactstrap";
+import DishDetail from "./DishdetailComponent";
+import { Card, CardImg, CardImgOverlay, CardTitle } from "reactstrap";
 
 class Menu extends Component {
   constructor(props) {
@@ -21,21 +16,21 @@ class Menu extends Component {
     this.setState({ selectedDish: dish });
   }
 
-<<<<<<< HEAD
   onDishComments(dishComnts) {
-    this.setState({dishComments : dishComnts});
+    this.setState({ dishComments: dishComnts });
   }
 
-=======
->>>>>>> 5a94c9b46a05f53077a4352e1d0ebbd1dde27592
   render() {
     const menu = this.props.dishes.map((dish) => {
       return (
         <div className="col-12 col-md-5 m-1">
-          <Card key={dish.id} onClick={() => {
-            this.onDishSelect(dish);
-            this.onDishComments(dish.comments)
-          }}>
+          <Card
+            key={dish.id}
+            onClick={() => {
+              this.onDishSelect(dish);
+              this.onDishComments(dish.comments);
+            }}
+          >
             <CardImg width="100%" src={dish.image} alt={dish.name} />
             <CardImgOverlay>
               <CardTitle>{dish.name}</CardTitle>
@@ -48,11 +43,10 @@ class Menu extends Component {
     return (
       <div className="container">
         <div className="row">{menu}</div>
-<<<<<<< HEAD
-        <DishDetail selectedDish = {this.state.selectedDish} comments = {this.state.dishComments}/>
-=======
-        <DishDetail selectedDish = {this.state.selectedDish} />
->>>>>>> 5a94c9b46a05f53077a4352e1d0ebbd1dde27592
+        <DishDetail
+          selectedDish={this.state.selectedDish}
+          comments={this.state.dishComments}
+        />
       </div>
     );
   }
