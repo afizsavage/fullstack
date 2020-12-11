ffba123b2348d577198e48168c53dcdd856132e2
Afiz Savage <savageafiz9@gmail.com>
2020-12-11 12:04:05 +0000

    Express Router

--- NodeJs/node-express/index.js
+++ NodeJs/node-express/index.js
@@ -3,41 +3,18 @@ const http = require("http");
 const morgan = require("morgan");
 const bodyParser = require("body-parser");
 
+const dishRouter = require("./routes/dishRouter");
+
 const hostname = "localhost";
 const port = 3000;
 
 const app = express();
-//used to log information to the screen
 app.use(morgan("dev"));
 app.use(bodyParser.json());
 
-app.all("/dishes", (req, res, next) => {
-  req.statusCode = 200;
-  res.setHeader("Content-Type", "text/plain");
-  next();
-});
+app.use("/dishes", dishRouter);
 
-app.get("/dishes", (req, res, next) => {
-  res.end("Will send all the dishes to you!");
-});
-
-app.post("/dishes", (req, res, next) => {
-  res.end(
-    "Will add the dish: " +
-      req.body.name +
-      " with details: " +
-      req.body.description
-  );
-});
-
-app.put("/dishes", (req, res, next) => {
-  res.statusCode = 403;
-  res.end("PUT operation not supported on /dishes");
-});
-
-app.delete("/dishes", (req, res, next) => {
-  res.end("Deleting all dishes");
-});
+app.use(express.static(__dirname + "/public"));
 
 app.get("/dishes/:dishId", (req, res, next) => {
   res.end("Will send details of the dish: " + req.params.dishId + " to you!");
@@ -62,15 +39,11 @@ app.delete("/dishes/:dishId", (req, res, next) => {
   res.end("Deleting dish: " + req.params.dishId);
 });
 
-app.use(express.static(__dirname + "/public"));
-
 app.use((req, res, next) => {
   res.statusCode = 200;
   res.setHeader("Content-Type", "text/html");
   res.end("<html><body><h1>This is an Express Server</h1></body></html>");
 });
-
-app.use(express.static(__dirname + "/public"));
 
 const server = http.createServer(app);
 
